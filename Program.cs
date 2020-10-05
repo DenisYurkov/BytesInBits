6f05883266e950be32ddc34e9c3c8f003982648d
Denis Yurkov <denisdeveloper136@gmail.com>
2020-10-05 20:49:19 +0200

    Add Project!

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ByteInBits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----Byte In Bits---");
+            Console.Write("Write bytes: ");
+           
+            int bytes = Convert.ToInt32(Console.ReadLine());
+            int bits = 0;
+
+            if (bytes != 0)
+            {
+                if (bytes == 1)
+                {
+                    bits = 8;
+                    Console.WriteLine("{0} Bytes: {1} Bits.", bytes, bits);
+                    Console.ReadLine(); // does not close the application until the user presses enter.
+                }
+                else 
+                {
+                    bits = 8;
+                    bits *= bytes;
+                    Console.WriteLine("{0} Bytes: {1} Bits.", bytes, bits);
+                    Console.ReadLine(); // does not close the application until the user presses enter.
+                }
+            }
+        }
+    }
+}
